f27ee084ab6a88a5992c4fd122ec6cffc134f167
jayeshthakare <jayesh.thakare87@gmail.com>
2025-09-04 16:28:18 +0530

    Syntax and Indentation

--- Indentation _excercise.py
+++
@@ -1,7 +0,0 @@
-#Indentation block execution
-if True:
-    print("i am inside 1st indentation")
-    if False:
-        print("i am 2nd indentation")
-    print("i am again in 1st indentation")
-print("hello all")
